1abcf2d581803ebf200ad6185a4ae1a6afb3bae8
Deepak Rawat <dev.deepak.rawat@gmail.com>
2022-04-07 11:55:45 +0400

    Add authentication

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+  def login; end
+
+  def create
+    @user = User.find_by(username: params[:username])
+
+    ## authenticate user credentials
+    if @user && @user.authenticate(params[:password])
+      # set session and redirect on success
+      session[:user_id] = @user.id
+      redirect_to @user
+    else
+      # error message on fail
+      message = 'Something went wrong! Make sure your username and password are correct.'
+      redirect_to login_url, notice: message
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to login_url
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root 'sessions#home'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # user routes
+  resources :users, only: %i[new create edit update show destroy]
+
+  # session routes
+  get '/login', to: 'sessions#login'
+  post '/login', to: 'sessions#create'
+  post '/logout', to: 'sessions#destroy'
+  get '/logout', to: 'sessions#destroy'
+  delete '/sessions', to: 'sessions#destroy'
 end
